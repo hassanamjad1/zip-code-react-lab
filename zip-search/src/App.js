9fa98d13290950777b25cf4bcb514550705c4dc4
hassanamjad1 <42243986+hassanamjad1@users.noreply.github.com>
2021-10-20 00:44:11 -0400

    Adding Event Handlers

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -7,19 +7,47 @@ function City(props) {
 }
 
 function ZipSearchField(props) {
-  return (<div>This is the ZipSearchField component</div>);
+  return (
+  <div>
+    Enter Zip Code : 
+    <input type="text" onChange={ props.changeHandler } />
+  </div>
+  
+  );
 }
 
 
 class App extends Component {
+
+  state = {
+    zipCode :'',
+    cities : [],
+  }
+  zipChanged = (event) => {
+    // console.log(event.target.value)
+    this.setState({ zipCode : event.target.value})
+    console.log(this.state.zipCode)
+
+    fetch('http://ctp-zip-api.herokuapp.com/zip/'+event.target.value)
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Zip Code Search</h2>
         </div>
-        <ZipSearchField />
+        
+        <ZipSearchField zipCode={this.state.zipCode} changeHandler={this.zipChanged} />
+        <div>Current Zip is {this.state.zipCode}</div>
+        {/* <div>Current cities is {this.state.cities}</div> */}
+
         <div>
+          { this.state.cities.map( (city) => <City/>)}
+          <City />
           <City />
           <City />
         </div>
